1393f50b199720325c77fd32dd17b683740be6fb
Aslak Knutsen <aslak@4fs.no>
2017-02-22 04:34:14 +0100

    Add text and hist report

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-vegeta attack --duration ${DURATION} --rate ${RATE} --targets targets | vegeta report -reporter='hist[0,20ms,40ms,60ms,100ms,200ms,1s,2s,3s]'
+vegeta attack --duration ${DURATION} --rate ${RATE} --targets targets > result.bin
+vegeta report -inputs result.bin
+vegeta report -inputs result.bin -reporter='hist[0,20ms,40ms,60ms,100ms,200ms,1s,2s,3s]'
